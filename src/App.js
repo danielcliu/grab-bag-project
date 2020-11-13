1480f19d89fef842695aba4a5ce22f7b95f3f77f
danielcliu <dcliu@ucdavis.edu>
2020-11-12 20:09:53 -0800

    Split html into better groups, created Bag class

--- src/App.js
+++ src/App.js
@@ -9,108 +9,42 @@ import {
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import {FaTrash, FaTrashRestore} from "react-icons/fa";
 import "./App.css";
+import Bag from "./Bag.js";
 
 class Collection extends React.Component{
 	
 	constructor(props){
 		super(props);
 
-		this.imageClick = this.imageClick.bind(this);
-		this.switchGrabBagPageDown = this.switchGrabBagPageDown.bind(this);
-		this.switchGrabBagPageUp = this.switchGrabBagPageUp.bind(this);
-		this.switchIFIBagPageDown = this.switchIFIBagPageDown.bind(this);
-		this.switchIFIBagPageUp = this.switchIFIBagPageUp.bind(this);
-	}
-	imageClick(item) {
-		this.props.selectGBDevice(item);
-	}
-	
-	switchGrabBagPageDown(){
-		this.props.changeGBPage(-1)
-	}
-	
-	switchGrabBagPageUp(){
-		this.props.changeGBPage(+1)
-	}
-	
-	switchIFIBagPageDown(){
-		this.props.changeIFIPage(-1)
-	}
-	
-	switchIFIBagPageUp(){
-		this.props.changeIFIPage(+1)
 	}
 
 	render(){
 	  console.log("P{RPS: ", this.props);
-	  //console.log(this.props.iFixitBag);
 	  return (
 		  <div>
 		      <GridContextProvider onChange={this.props.onChange}>
 		        <div className="container">
-		        <div className="device-bag">  
-		      		<Search handleSubmit={this.props.handleSubmit}/>
-				<GridDropZone
-				    className="dropzone iFixitBag"
-				    id="iFixitBag"
-				    boxesPerRow={4}
-				    rowHeight={70}
-				  >
-				    {this.props.iFixitBag.devices.map(item => (
-							<GridItem key={item.id}>
-							  <div className="grid-item">
-							    <img className="grid-item-content grid-item-image" src={item.image} alt={item.display_name}/>
-							  </div>
-							</GridItem>
-						      ))}
-				  </GridDropZone>
-				    <div className="pager">
-					<button onClick={this.switchIFIBagPageDown}>
-						<FiChevronLeft />
-					</button>
-					<span>
-						Page {this.props.iFixitBag.page+1}
-					</span>
-					<button onClick={this.switchIFIBagPageUp}>
-						<FiChevronRight/>
-					</button>
-				    </div>
-		  	</div>
-		  	<div className="device-bag">
-				  <GridDropZone
-				    className="dropzone grabBag"
-				    id="grabBag"
-				    boxesPerRow={4}
-				    rowHeight={70}
-				  >
-				    {this.props.grabBag.devices[this.props.grabBag.page].map(item => (
-							<GridItem key={item.id}>
-							  <div className="grid-item">
-							    <img className="grid-item-content grid-item-image" src={item.image} alt={item.display_title} onDoubleClick={() => this.imageClick(item)}/>
-							   </div>
-							</GridItem>
-						      ))}
-				  </GridDropZone>
-				    <div className="pager">
-					<button onClick={this.switchGrabBagPageDown}>
-						<FiChevronLeft />
-					</button>
-					<span>
-						Page {this.props.grabBag.page+1}
-					</span>
-					<button onClick={this.switchGrabBagPageUp}>
-						<FiChevronRight/>
-					</button>
-				    </div>
-				  <GridDropZone
-				    className="drop-trash trash"
-				    id="trash"
-				    boxesPerRow={1}
-				    rowHeight={4}
-				  >
-					<FaTrash class="fa-trash" />
-				  </GridDropZone>
-		  	</div>
+				<Search handleSubmit={this.props.handleSubmit}/>
+				<Bag bagName="iFixitBag" 
+					bagDevices={this.props.iFixitBag.devices} 
+					bagPage={this.props.iFixitBag.page} 
+					changeBagPage={this.props.changeIFIPage} 
+					selectDevice={this.props.selectGBDevice} />
+				<div>
+				<Bag bagName="grabBag" 
+					bagDevices={this.props.grabBag.devices[this.props.grabBag.page]} 
+					bagPage={this.props.grabBag.page} 
+					changeBagPage={this.props.changeGBPage} 
+					selectDevice={this.props.selectGBDevice} />
+					  <GridDropZone
+					    className="drop-trash trash"
+					    id="trash"
+					    boxesPerRow={1}
+					    rowHeight={4}
+					  >
+						<FaTrash class="fa-trash" />
+					  </GridDropZone>
+				</div>
 		  	</div>
 		      </GridContextProvider>
 		  </div>
@@ -130,11 +64,13 @@ class Search extends React.Component{
 	handleChange(event){
 		this.setState({searchString : event.target.value});
 	}
+	
 	handleSubmit(event){
 		event.preventDefault();
 		console.log("here");
 		this.props.handleSubmit(this.state.searchString);
 	}
+	
 	render(){
 		return(	
 		<form onSubmit={this.handleSubmit}>
