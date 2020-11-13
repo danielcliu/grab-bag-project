be2c4a534cda0d8949de57f5e75e8f245203f521
danielcliu <dcliu@ucdavis.edu>
2020-11-12 17:49:17 -0800

    Implement Paging

--- src/App.js
+++ src/App.js
@@ -7,24 +7,38 @@ import {
 	  move
 } from "react-grid-dnd";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import {FaTrash, FaTrashRestore} from "react-icons/fa";
 import "./App.css";
 
-		  	    /*<div>
-		  		<button>
-		  			<FiChevronLeft />
-		  		</button>
-		  		<span>
-		  			Page {this.props.grabBag.page}
-		  		</span>
-		  		<button>
-		  			<FiChevronRight/>
-		  	    	</button>
-		  	    </div>
-		  	   */
 class Collection extends React.Component{
 	
+	constructor(props){
+		super(props);
+
+		this.imageClick = this.imageClick.bind(this);
+		this.switchGrabBagPageDown = this.switchGrabBagPageDown.bind(this);
+		this.switchGrabBagPageUp = this.switchGrabBagPageUp.bind(this);
+		this.switchIFIBagPageDown = this.switchIFIBagPageDown.bind(this);
+		this.switchIFIBagPageUp = this.switchIFIBagPageUp.bind(this);
+	}
 	imageClick(item) {
 		this.props.selectGBDevice(item);
+	}
+	
+	switchGrabBagPageDown(){
+		this.props.changeGBPage(-1)
+	}
+	
+	switchGrabBagPageUp(){
+		this.props.changeGBPage(+1)
+	}
+	
+	switchIFIBagPageDown(){
+		this.props.changeIFIPage(-1)
+	}
+	
+	switchIFIBagPageUp(){
+		this.props.changeIFIPage(+1)
 	}
 
 	render(){
@@ -32,46 +46,72 @@ class Collection extends React.Component{
 	  //console.log(this.props.iFixitBag);
 	  return (
 		  <div>
-		      <Search handleSubmit={this.props.handleSubmit}/>
 		      <GridContextProvider onChange={this.props.onChange}>
 		        <div className="container">
-		          <GridDropZone
-		            className="dropzone iFixitBag"
-		            id="iFixitBag"
-		            boxesPerRow={4}
-		            rowHeight={70}
-		          >
-		            {this.props.iFixitBag.devices.map(item => (
-				                <GridItem key={item.id}>
-				                  <div className="grid-item">
-				                    <img className="grid-item-content grid-item-image" src={item.image} alt={item.display_name}/>
-				                  </div>
-				                </GridItem>
-				              ))}
-		          </GridDropZone>
-		          <GridDropZone
-		            className="dropzone grabBag"
-		            id="grabBag"
-		            boxesPerRow={4}
-		            rowHeight={70}
-		          >
-		            {this.props.grabBag.devices[this.props.grabBag.page].map(item => (
-				                <GridItem key={item.id}>
-				                  <div className="grid-item">
-				                    <img className="grid-item-content grid-item-image" src={item.image} alt={item.display_title} onDoubleClick={() => this.imageClick(item)}/>
-				    		   </div>
-				                </GridItem>
-				              ))}
-		          </GridDropZone>
+		        <div className="device-bag">  
+		      		<Search handleSubmit={this.props.handleSubmit}/>
+				<GridDropZone
+				    className="dropzone iFixitBag"
+				    id="iFixitBag"
+				    boxesPerRow={4}
+				    rowHeight={70}
+				  >
+				    {this.props.iFixitBag.devices.map(item => (
+							<GridItem key={item.id}>
+							  <div className="grid-item">
+							    <img className="grid-item-content grid-item-image" src={item.image} alt={item.display_name}/>
+							  </div>
+							</GridItem>
+						      ))}
+				  </GridDropZone>
+				    <div className="pager">
+					<button onClick={this.switchIFIBagPageDown}>
+						<FiChevronLeft />
+					</button>
+					<span>
+						Page {this.props.iFixitBag.page+1}
+					</span>
+					<button onClick={this.switchIFIBagPageUp}>
+						<FiChevronRight/>
+					</button>
+				    </div>
 		  	</div>
-		          <GridDropZone
-		            className="dropzone trash"
-		            id="trash"
-		            boxesPerRow={1}
-		            rowHeight={4}
-		          >
-		  		<div> Trash </div>
-		  	  </GridDropZone>
+		  	<div className="device-bag">
+				  <GridDropZone
+				    className="dropzone grabBag"
+				    id="grabBag"
+				    boxesPerRow={4}
+				    rowHeight={70}
+				  >
+				    {this.props.grabBag.devices[this.props.grabBag.page].map(item => (
+							<GridItem key={item.id}>
+							  <div className="grid-item">
+							    <img className="grid-item-content grid-item-image" src={item.image} alt={item.display_title} onDoubleClick={() => this.imageClick(item)}/>
+							   </div>
+							</GridItem>
+						      ))}
+				  </GridDropZone>
+				    <div className="pager">
+					<button onClick={this.switchGrabBagPageDown}>
+						<FiChevronLeft />
+					</button>
+					<span>
+						Page {this.props.grabBag.page+1}
+					</span>
+					<button onClick={this.switchGrabBagPageUp}>
+						<FiChevronRight/>
+					</button>
+				    </div>
+				  <GridDropZone
+				    className="drop-trash trash"
+				    id="trash"
+				    boxesPerRow={1}
+				    rowHeight={4}
+				  >
+					<FaTrash class="fa-trash" />
+				  </GridDropZone>
+		  	</div>
+		  	</div>
 		      </GridContextProvider>
 		  </div>
 		  );
--- src/GrabBag.js
+++ src/GrabBag.js
@@ -4,22 +4,25 @@ import {
 	  swap,
 	  move
 } from "react-grid-dnd";
-import iFixitApi from './iFixitApi'
+import iFixitApi from './iFixitApi';
+import _ from "lodash";
 
 class GrabBag extends React.Component{
 	constructor(props) {
     		super(props);
 		const grabBagSavedDevices = JSON.parse(sessionStorage.getItem('grabBagDevices')) || [[]];
     		this.state = {
-			limit: 4,
-			
+			limit: 20,
+			searchString: '',	
 			iFixitBag:{'devices': [], 'page': 0, 'selected': {'display_title': '', 'url': ''}},
 			grabBag: {'devices': grabBagSavedDevices, 'page': 0, 'selected': {'display_title': '', 'url': ''}},
 			grabBagDevices: grabBagSavedDevices,
 			trash:{'devices': [], 'page': 0 },
 		};
 		this.onChange = this.onChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
+		this.changeGBPage = this.changeGBPage.bind(this);
+		this.changeIFIPage = this.changeIFIPage.bind(this);
+		this.handleSearch = this.handleSearch.bind(this);
 		this.selectGBDevice = this.selectGBDevice.bind(this);
  	 }
 	 
@@ -53,15 +56,15 @@ class GrabBag extends React.Component{
 		});
 	}
 	
-	async handleSubmit(text){
-		const devices = await iFixitApi.get(`search/${text}?filter=category&limit=${this.state.limit}&offset=${this.state.iFixitBag.page}`).then(response => {
+	async handleSearch(text){
+		let localState = _.cloneDeep(this.state.iFixitBag);
+		const devices = await iFixitApi.get(`search/${text}?filter=category&limit=${this.state.limit}&offset=${0}`).then(response => {
 			return Array.from(response.data.results, device => {return { 'display_title': device.display_title, 'id': device.wikiid, 'image': device.image.standard, 'url': device.url}}); 
 			});
 		console.log(devices);
-	 	this.setState(prevState => {
-			prevState.iFixitBag.devices = devices;
-			return	{iFixitBag: prevState.iFixitBag}}
-		);	
+		localState.devices = devices;
+		localState.page = 0;
+	 	this.setState({iFixitBag: localState, searchString: text});	
 		console.log(text);
 	}
 	
@@ -79,28 +82,62 @@ class GrabBag extends React.Component{
 	}
 
 	redistributeGBDevices(stateDevices){
-		console.log("state Devices: ", stateDevices);
-		const deepCopy = stateDevices;
+		//console.log("state Devices: ", stateDevices);
+		const deepCopy = _.cloneDeep(stateDevices);
 		let newDevices = []
 		let flatCopy = deepCopy.flat();
-		console.log("howdy ", flatCopy);
+		//console.log("howdy ", flatCopy);
 		let flatLen = flatCopy.length;
-		console.log("howdy ", flatLen);
+		//console.log("howdy ", flatLen);
 		for (let i = 0; i < flatLen; i += this.state.limit){
 			let itemList = [];
-			console.log("limit: ", this.state.limit, " i: ", i);
+		//	console.log("limit: ", this.state.limit, " i: ", i);
 			for (let j = 0; j < this.state.limit; j++){
-				console.log("in inner loop");
-				if(j+i == flatLen){break;}
-				console.log("about to push loop", );
+		//		console.log("in inner loop");
+				if(j+i === flatLen){break;}
+		//		console.log("about to push loop", );
 				itemList.push(flatCopy[i+j]);
 			
 			} 
-			console.log("IN LOOP x: ", i, ' ', itemList);
+		//	console.log("IN LOOP x: ", i, ' ', itemList);
 			newDevices.push(itemList);
 		}
+		if (newDevices.length===0) newDevices.push([]);
 		console.log("NEW DEVICES: ", newDevices);
 		return newDevices;
+	}
+
+	changeGBPage(diff){
+		//console.log("IN GB Pasge Change", diff);
+		let nextPage = this.state.grabBag.page + diff;
+		//console.log("IN NextPage ", nextPage, ' and; ', this.state.grabBag.devices.length -1);
+		if(nextPage >= 0 && nextPage <= (this.state.grabBag.devices.length -1)){
+			this.setState(prevState => {
+				prevState.grabBag.page = nextPage;
+				return {'grabBag': prevState.grabBag}})
+		}
+		else{ console.log("Not a legit page");}
+	}
+
+	async changeIFIPage(diff){
+		let localState = _.cloneDeep(this.state.iFixitBag);
+		localState.page += diff;
+		const offset = localState.page * this.state.limit;
+		if (localState.page <0){return 0;} 
+		if(this.state.searchString === ''){
+			const devices = await this.getDeviceList(offset, this.state.limit);
+			localState.devices = devices
+			this.setState({ iFixitBag: localState})
+		}
+		else{
+			const devices = await iFixitApi.get(`search/${this.state.searchString}?filter=category&limit=${this.state.limit}&offset=${offset}`).then(response => {
+				return Array.from(response.data.results, device => {return { 'display_title': device.display_title, 'id': device.wikiid, 'image': device.image.standard, 'url': device.url}}); 
+			});
+			localState.devices = devices;
+			console.log(devices);
+			this.setState({iFixitBag: localState});	
+		}	
+	
 	}
 
 	onChange(sourceId, sourceIndex, targetIndex, targetId) {
@@ -131,14 +168,22 @@ class GrabBag extends React.Component{
 				    sourceIndex,
 				    targetIndex
 				  );
+				const deepCopy = _.cloneDeep(this.state[sourceId]);
+
+				deepCopy.devices[deepCopy.page] = result[0];
+					
+				let newDevices = this.redistributeGBDevices(deepCopy.devices);
+				let page;
+				if (Math.max(newDevices.length-1, 0) < deepCopy.page){
+					deepCopy.page -=  1;
+					console.log("DC we should jump to page: ", page);
+				}
+				deepCopy.devices = newDevices;
 				return this.setState(prevState => {
-					let localState = prevState;
 					prevState.grabBagDevices[prevState[sourceId].page] = result[0];
 					
 					let newDevices = this.redistributeGBDevices(prevState.grabBagDevices);
-
-					localState[sourceId].devices = newDevices;
-					return {[sourceId]: localState[sourceId], 'grabBagDevices': newDevices}
+					return {[sourceId]: deepCopy, 'grabBagDevices': newDevices}
 			  	});
 
 			} 
@@ -151,6 +196,9 @@ class GrabBag extends React.Component{
 				return this.setState(prevState => {
 					console.log(prevState);
 					prevState[sourceId].devices[prevState[sourceId].page] = result;
+					if (result.length === 0){
+						
+					}
 					prevState.grabBagDevices[prevState[sourceId].page] = result;
 					return {[sourceId]: prevState[sourceId], 'grabBagDevices': prevState.grabBagDevices}
 			  	});
@@ -175,13 +223,12 @@ class GrabBag extends React.Component{
 					grabBag={this.state.grabBag}
 					grabBagDevices={this.state.grabBagDevices}
 					onChange={this.onChange}
-					handleSubmit={this.handleSubmit}
+					handleSubmit={this.handleSearch}
+					changeGBPage={this.changeGBPage}
+					changeIFIPage={this.changeIFIPage}
 					selectGBDevice={this.selectGBDevice}
 				/>
-				<button onClick={() => console.log(this.state)}>
-				  Click me
-				</button>
-				<a href={this.state.grabBag.selected.url} target="_blank" >{this.state.grabBag.selected.display_title}</a>
+				<a className="pager" href={this.state.grabBag.selected.url} target="_blank" >{this.state.grabBag.selected.display_title}</a>
 			</div>
 		)
 	}
