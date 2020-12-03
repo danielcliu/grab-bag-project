95c4086b2b2aba5b33ce07058f24b72d65d87f56
danielcliu <dcliu@ucdavis.edu>
2020-12-02 23:27:01 -0800

    Ran prettier on src files

--- src/Bag.js
+++ src/Bag.js
@@ -1,66 +1,62 @@
 import React from "react";
-import {
-	  GridDropZone,
-	  GridItem,
-} from "react-grid-dnd";
+import { GridDropZone, GridItem } from "react-grid-dnd";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
-class Bag extends React.Component{
-	
-	constructor(props){
-		super(props);
+class Bag extends React.Component {
+  constructor(props) {
+    super(props);
 
-		this.imageClick = this.imageClick.bind(this);
-		this.switchBagPageDown = this.switchBagPageDown.bind(this);
-		this.switchBagPageUp = this.switchBagPageUp.bind(this);
-	}
-	
-	imageClick(item) {
-		this.props.selectDevice(item);
-	}
-	
-	switchBagPageDown(){
-		this.props.changeBagPage(-1)
-	}
-	
-	switchBagPageUp(){
-		this.props.changeBagPage(+1)
-	}
-	
+    this.imageClick = this.imageClick.bind(this);
+    this.switchBagPageDown = this.switchBagPageDown.bind(this);
+    this.switchBagPageUp = this.switchBagPageUp.bind(this);
+  }
 
+  imageClick(item) {
+    this.props.selectDevice(item);
+  }
 
-	render(){
-	  return (
-		        <div className="device-bag">  
-				<GridDropZone
-				    className={`dropzone ${this.props.bagName}`}
-				    id={this.props.bagName}
-				    boxesPerRow={4}
-				    rowHeight={70}
-				  >
-				    {this.props.bagDevices.map(item => (
-							<GridItem key={item.id}>
-							  <div className="grid-item">
-							    <img className="grid-item-content grid-item-image" src={item.image} alt={item.display_name} onClick={() => this.imageClick(item)} />
-							  </div>
-							</GridItem>
-						      ))}
-				  </GridDropZone>
-				    <div className="pager">
-					<button onClick={this.switchBagPageDown}>
-						<FiChevronLeft />
-					</button>
-					<span>
-						Page {this.props.bagPage+1}
-					</span>
-					<button onClick={this.switchBagPageUp}>
-						<FiChevronRight/>
-					</button>
-				    </div>
-		  	</div>
-	
-	  );
-	}
+  switchBagPageDown() {
+    this.props.changeBagPage(-1);
+  }
+
+  switchBagPageUp() {
+    this.props.changeBagPage(+1);
+  }
+
+  render() {
+    return (
+      <div className="device-bag">
+        <GridDropZone
+          className={`dropzone ${this.props.bagName}`}
+          id={this.props.bagName}
+          boxesPerRow={4}
+          rowHeight={70}
+        >
+          {this.props.bagDevices.map((item) => (
+            <GridItem key={item.id}>
+              <div className="grid-item">
+                <img
+                  className="grid-item-content grid-item-image"
+                  src={item.image}
+                  alt={item.display_name}
+                  onClick={() => this.imageClick(item)}
+                />
+              </div>
+            </GridItem>
+          ))}
+        </GridDropZone>
+        <div className="pager">
+          <button onClick={this.switchBagPageDown}>
+            <FiChevronLeft />
+          </button>
+          <span>Page {this.props.bagPage + 1}</span>
+          <button onClick={this.switchBagPageUp}>
+            <FiChevronRight />
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Bag;
--- src/Search.js
+++ src/Search.js
@@ -1,76 +1,79 @@
 import React from "react";
 import "./App.css";
 import "./Search.css";
-import iFixitApi from './iFixitApi';
-import Autosuggest from 'react-autosuggest';
-import { FaSearch } from 'react-icons/fa';
+import iFixitApi from "./iFixitApi";
+import Autosuggest from "react-autosuggest";
+import { FaSearch } from "react-icons/fa";
 
-const getSuggestionValue = suggestion => suggestion.display_title;
+const getSuggestionValue = (suggestion) => suggestion.display_title;
 
-const renderSuggestion = suggestion => (
-	<span>{suggestion.display_title}</span>
+const renderSuggestion = (suggestion) => (
+  <span>{suggestion.display_title}</span>
 );
 
-const renderInputComponent = inputProps => (
-	  <div className="inputContainer">
-	    <FaSearch className="icon" />
-	    <input {...inputProps} />
-	  </div>
+const renderInputComponent = (inputProps) => (
+  <div className="inputContainer">
+    <FaSearch className="icon" />
+    <input {...inputProps} />
+  </div>
 );
 
-class SearchBar extends React.Component{
-	constructor(props){
-		super(props);
-		this.state = {value : '', suggestions:[]};
-	}
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "", suggestions: [] };
+  }
 
-	onChange = (event, { newValue }) => {
-		this.setState({ value: newValue	});
-	};
+  onChange = (event, { newValue }) => {
+    this.setState({ value: newValue });
+  };
 
-	onSuggestionsFetchRequested = async ({ value }) => {
-		let newSuggestions = await iFixitApi.get(`suggest/${value}?doctypes=category`).then(response => { 
-			return response.data.results.map( device => ({ display_title: device.display_title, id: device.wikiid}));
-		});
-		this.setState({
-		  suggestions: newSuggestions
-		});
-	};
-	
-	onSuggestionsClearRequested = () => {
-		this.setState({
-	  		suggestions: []
-		});
-		};
-	
+  onSuggestionsFetchRequested = async ({ value }) => {
+    let newSuggestions = await iFixitApi
+      .get(`suggest/${value}?doctypes=category`)
+      .then((response) => {
+        return response.data.results.map((device) => ({
+          display_title: device.display_title,
+          id: device.wikiid,
+        }));
+      });
+    this.setState({
+      suggestions: newSuggestions,
+    });
+  };
 
-	handleSubmit = (event) => {
-		event.preventDefault();
-		this.props.handleSubmit(this.state.value);
-	};
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: [],
+    });
+  };
 
-	render(){
-		const { value, suggestions } = this.state;
-		const inputProps = {
-			      placeholder: 'Search iFixit Devices',
-			      value,
-			      onChange: this.onChange
-			    };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.handleSubmit(this.state.value);
+  };
 
-		return(	
-		<form className="search-bar" onSubmit={this.handleSubmit}>
-			<Autosuggest
-			        suggestions={suggestions}
-			        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-			        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-				getSuggestionValue={getSuggestionValue}
-			        renderSuggestion={renderSuggestion}
-			        inputProps={inputProps}
-			      	renderInputComponent={renderInputComponent}
-			/>
-		</form>
-		);
-	}
+  render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: "Search iFixit Devices",
+      value,
+      onChange: this.onChange,
+    };
 
+    return (
+      <form className="search-bar" onSubmit={this.handleSubmit}>
+        <Autosuggest
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+          getSuggestionValue={getSuggestionValue}
+          renderSuggestion={renderSuggestion}
+          inputProps={inputProps}
+          renderInputComponent={renderInputComponent}
+        />
+      </form>
+    );
+  }
 }
 export default SearchBar;
